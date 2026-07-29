3a027de5ef24637541ad701a6402036feaee0c9b
nodashuto <nodashuto@proton.me>
2026-07-29 19:25:39 +0900

    change level.rs
    init level according w/ first person view model from bevy example

--- src/game/level/level.rs
+++ src/game/level/level.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
 use bevy_rapier3d::prelude::*;
+use bevy::{
+ color::palettes::tailwind,   
+};
 
 
 pub struct LevelPlugin;
@@ -11,15 +14,47 @@ impl Plugin for LevelPlugin {
     }
 }
 
+/// Used implicitly by all entities without a `RenderLayers` component.
+/// Our world model camera and all objects other than the player are on this layer.
+/// The light source belongs to both layers.
+const DEFAULT_RENDER_LAYER: usize = 0;
+
+/// Used by the view model camera and the player's arm.
+/// The light source belongs to both layers.
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
+
 fn init_level(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let level_material = materials.add(StandardMaterial {
-        base_color: Color::WHITE,
-        ..default()
-    });
+    let floor = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(10.0)));
+    let cube = meshes.add(Cuboid::new(2.0, 0.5, 1.0));
+    let material = materials.add(Color::WHITE);
 
+    commands.spawn((Mesh3d(floor), MeshMaterial3d(material.clone())));
+
+    commands.spawn((
+        Mesh3d(cube.clone()),
+        MeshMaterial3d(material.clone()),
+        Transform::from_xyz(0.0, 0.25, -3.0),
+    ));
+
+    commands.spawn((
+        Mesh3d(cube),
+        MeshMaterial3d(material),
+        Transform::from_xyz(0.75, 1.75, 0.0),
+    ));
+
+        commands.spawn((
+        PointLight {
+            color: Color::from(tailwind::ROSE_300),
+            shadow_maps_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(-2.0, 4.0, -0.75),
+        // The light source illuminates both the world model and the view model.
+  
+    ));
 
 }
