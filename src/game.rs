0adf65d0be34536486453447496c451828c7dbb9
nodashuto <nodashuto@proton.me>
2026-07-29 14:47:55 +0900

    refac: separate main.rs into game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,30 @@
-use bevy::prelude::*;
+use bevy::{
+    color::palettes::basic::PURPLE,
+    prelude::*,
+};
+
 
 pub struct GamePlugin;
 
-
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
+	app.add_systems(Startup, setup);
 	
     }
+        // Register your systems here
+}
+
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Mesh2d(meshes.add(Rectangle::default())),
+        MeshMaterial2d(materials.add(Color::from(PURPLE))),
+        Transform::default().with_scale(Vec3::splat(128.)),
+    ));
 }
