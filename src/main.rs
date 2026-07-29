b6e65217377771b20bc63e8122a4a38c04d655bf
nodashuto <nodashuto@proton.me>
2026-07-29 15:56:15 +0900

    change game.rs, player.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use bevy::prelude::*;
 
-
-use game::GamePlugin;
-
 // This line tells the compiler to include the code it finds in src/game.rs
 pub mod game;
 
@@ -10,7 +7,7 @@ pub mod game;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(GamePlugin)
+        .add_plugins(game::game::GamePlugin)
         .run();
 }
 
